c655d03823db9c919db87fa7ab137ed4937456e3
Daniel Eyßer <daniel.eysser@gmail.com>
2020-03-08 17:17:30 +0100

    st: update config.h

--- srcpkgs/st/files/config.h
+++ srcpkgs/st/files/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=15:antialias=true:autohint=false";
+static char *font = "LiterationMono Nerd Font:pixelsize=15:antialias=true:autohint=false";
 static int borderpx = 2;
 
 /*
