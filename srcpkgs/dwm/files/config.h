a731f998bf40ace75f22feb06e6d65caa3a9a481
Daniel Eyßer <daniel.eysser@gmail.com>
2020-10-04 18:29:17 +0200

    dwm: update custom.patch

--- srcpkgs/dwm/files/config.h
+++ srcpkgs/dwm/files/config.h
@@ -50,13 +50,14 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[T]",      tile        },    /* first entry is default */
-	{ "[F]",      NULL        },    /* no layout function means floating behavior */
-	{ "[M]",      monocle     },
-	{ "TTT",      bstack      },
-	{ "===",      bstackhoriz },
-	{ "[@]",      spiral      },
-	{ "[\\]",     dwindle     },
+	{ "[T]",      tile           },    /* first entry is default */
+	{ "[F]",      NULL           },    /* no layout function means floating behavior */
+	{ "[M]",      monocle        },
+	{ "TTT",      bstack         },
+	{ "===",      bstackhoriz    },
+	{ "[@]",      spiral         },
+	{ "[\\]",     dwindle        },
+	{ "|M|",      centeredmaster },
 };
 
 /* key definitions */
@@ -134,6 +135,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_s,      setlayout,      {.v = &layouts[5]} },
 	{ MODKEY,                       XK_a,      setlayout,      {.v = &layouts[6]} },
+	{ MODKEY,                       XK_w,      setlayout,      {.v = &layouts[7]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
